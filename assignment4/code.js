c24fd2b3f6efae010654e6b70e63de6412e87f71
Varun <varunkallepally1@gmail.com>
2025-02-19 15:44:22 +0530

    assignment4

---
+++ assignment4/code.js
@@ -0,0 +1,11 @@
+console.log(x) //value is not allocated so it returns undefined
+var x = 2;
+
+myName();
+
+function myName() {
+console.log("VARUN")
+} 
+
+let y = 34; //value is not allocated so it returns undefined
+console.log(y)
