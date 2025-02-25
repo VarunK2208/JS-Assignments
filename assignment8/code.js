46b9b129ac19b1fb2a76c65c6f50fa4e958a3efc
Varun Kallepally <varun.k@snapperit.com>
2025-02-25 05:45:42 +0000

    Assignment8: changes made

--- assignment8/code.js
+++ assignment8/code.js
@@ -9,3 +9,7 @@ function a() {
 
 console.log(b)
 a();
+
+//function c is defined inside the parent function a 
+// in Global execution context, during memory allocation c accesses its parent function for the variables if it doesnt present in it.
+// then variable is assigned and runs the code
