b64fe22af5f20c0d69c028201d91dd5ccd753aa7
Varun Kallepally <varun.k@snapperit.com>
2025-02-25 05:51:09 +0000

    Assignment9: changes made

--- assignment9/code.js
+++ assignment9/code.js
@@ -1,11 +1,14 @@
-let a ;
-a= 10;
+let a ; //until the value is initilized it remain in TDZ
+a= 10;  //in TDZ
 console.log(a);
-console.log(b);
+console.log(b); // as var is a global variable it will return undefined.
 var b = 100;
 
 // let a = 13; //throws syntax error
 
-const ac = 1500;
+console.log(f) //throws reference error : as it not initialized
+let f = 12;
+
+const ac = 1500; // it doesnt throw any error as it is initilized
 console.log(ac);
 
