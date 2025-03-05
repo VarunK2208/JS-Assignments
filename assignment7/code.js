bde84cffcbbc76dd81569dcf4c0d93de32313cc3
VarunK2208 <varun.k@snapperit.com>
2025-03-05 16:20:28 +0530

    assignment7: adding assignment files (#7)

---
+++ assignment7/code.js
@@ -0,0 +1,11 @@
+console.log(a)
+var a =7; // it shows undefined bcz variable is declared after console.log..... acts as placeholder
+
+console.log(x) //it shows error not defined bcz we didnt define any variable
+
+var b ;
+if(b === undefined) {
+    console.log("undefined")
+} else {
+    console.log("not undefined")
+}
