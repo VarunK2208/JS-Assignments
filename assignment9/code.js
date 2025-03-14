2312a741007a14d9b1a12b39d1ea469973a62d11
Varun Kallepally <varun.k@snapperit.com>
2025-03-14 03:31:25 +0000

    Made changes

--- assignment9/code.js
+++ assignment9/code.js
@@ -1,5 +1,5 @@
-let a ; //until the value is initilized it remain in TDZ
-a= 10;  //in TDZ
+let a; //until the value is initilized it remain in TDZ
+a = 10;  //in TDZ
 console.log(a);
 console.log(b); // as var is a global variable it will return undefined.
 var b = 100;
@@ -11,4 +11,6 @@ let f = 12;
 
 const ac = 1500; // it doesnt throw any error as it is initilized
 console.log(ac);
+
+// The Temporal Dead Zone (TDZ) is the period between when a variable is hoisted (allocated memory) and when it is initialized with a value. During this time, trying to access the variable results in a ReferenceError.
 
